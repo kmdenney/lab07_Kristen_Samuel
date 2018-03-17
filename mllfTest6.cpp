7201e2f1f79a5bc983e4debcb92582c1184a97a8
Kristen Denney <kmdenney@umail.ucsb.edu>
2018-03-16 17:25:28 -0700

    Finished

--- mllfTest6.cpp
+++ mllfTest6.cpp
@@ -5,10 +5,33 @@
 using namespace std;
 
 int main() {
-  //Test your deleteNodeRecursively and deleteNodeIteratively functions
-  //Test your code extensively
-  assertTrue(false);
+
+  LinkedList emptyList = {NULL,NULL};
+  deleteNodeIteratively(&emptyList, 123);
+  ASSERT_EQUALS(0,sum(&emptyList));
+
+  int oneNum[1] = {42};
+  LinkedList * oneList = arrayToLinkedList(oneNum, 1);
+  deleteNodeIteratively(oneList, 14);
+  ASSERT_EQUALS("[42]->null", linkedListToString(oneList));
+  deleteNodeIteratively(oneList, 42);
+  ASSERT_EQUALS(0,sum(oneList));
+
+  int twoNum[2] = {42, 57};
+  LinkedList * twoList = arrayToLinkedList(twoNum, 2);
+  deleteNodeIteratively(twoList, 14);
+  ASSERT_EQUALS("[42]->[57]->null", linkedListToString(twoList));
+  deleteNodeIteratively(twoList, 57);
+  ASSERT_EQUALS("[42]->null", linkedListToString(twoList));
   
+  
+
+  int threeNums[3]={42,57,61};
+  LinkedList *list = arrayToLinkedList(threeNums,3);
+  deleteNodeIteratively(list, 61);
+  ASSERT_EQUALS("[42]->[57]->null", linkedListToString(list));
+  
+
   return 0;
 }
 
--- mllfTest7.cpp
+++ mllfTest7.cpp
@@ -5,9 +5,22 @@
 using namespace std;
 
 int main() {
-  //Test your insertNodeToSortedList function
-  //Test your code extensively
-  assertTrue(false);
+  
+  LinkedList emptyList = {NULL,NULL};
+  insertNodeToSortedList(&emptyList, 5);
+  ASSERT_EQUALS("[5]->null", linkedListToString(&emptyList));
+
+  int oneNum[1] = {42};
+  LinkedList * oneList = arrayToLinkedList(oneNum, 1);
+  insertNodeToSortedList(oneList, 50);
+  ASSERT_EQUALS("[42]->[50]->null", linkedListToString(oneList));
+  insertNodeToSortedList(oneList, 45);
+  ASSERT_EQUALS("[42]->[45]->[50]->null", linkedListToString(oneList));
+  insertNodeToSortedList(oneList, 44);
+  ASSERT_EQUALS("[42]->[44]->[45]->[50]->null", linkedListToString(oneList));
+  insertNodeToSortedList(oneList, 3);
+  ASSERT_EQUALS("[3]->[42]->[44]->[45]->[50]->null",
+		linkedListToString(oneList));
   
   return 0;
 }
